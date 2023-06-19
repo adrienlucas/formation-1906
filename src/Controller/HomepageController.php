d54c1851bd78ab62ebeecdb4789fc266a57d8e63
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2023-06-19 17:45:03 +0200

    Twig refacto

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,17 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     #[Route('/hello/{name}', name: 'app_hello', requirements: ['name' => '\w+'])]
-    public function __invoke(Request $request, string $name = null): JsonResponse
+    public function index(Request $request, string $name = null): Response
     {
-        return $this->json([
+        return $this->render('homepage/index.html.twig', [
             'message' => sprintf('Hello %s!', $name ?? $request->query->get('name', 'world')),
-            'path' => 'src/Controller/HomepageController.php',
         ]);
     }
 }
