b782c56f796ce5fcb36ed17bfc59d3b089b98077
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2023-06-20 16:50:58 +0200

    Modify detail page

--- src/Controller/MovieDetailsController.php
+++ src/Controller/MovieDetailsController.php
@@ -2,23 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieDetailsController extends AbstractController
 {
-    #[Route('/movie/details', name: 'app_movie_details')]
-    public function index(): Response
+    #[Route('/movie/details/{id}', name: 'app_movie_details')]
+    public function index(Movie $movie): Response
     {
         return $this->render('movie_details/index.html.twig', [
-            'movie' => [
-                'title' => 'Terminator 2',
-                'releasedAt' => new \DateTime('1991-07-01'),
-                'genres' => [
-                    'Action', 'Sci-Fi'
-                ]
-            ]
+            'movie' => $movie
         ]);
     }
 }
